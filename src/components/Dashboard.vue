3c74e07d6375968741b1851a0e0dfa5e179092c3
Hemanth Gokavarapu <hgokavarapu@zendesk.com>
2019-01-26 17:53:45 -0800

    Script2 and router example for vue.js

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="container body">
+      <div class="main_container">
+        <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+              <h3>General</h3>
+              <ul class="nav side-menu">
+                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="index.html">Dashboard</a></li>
+                    <li><a href="index2.html">Dashboard2</a></li>
+                    <li><a href="index3.html">Dashboard3</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="form.html">General Form</a></li>
+                    <li><a href="form_advanced.html">Advanced Components</a></li>
+                    <li><a href="form_validation.html">Form Validation</a></li>
+                    <li><a href="form_wizards.html">Form Wizard</a></li>
+                    <li><a href="form_upload.html">Form Upload</a></li>
+                    <li><a href="form_buttons.html">Form Buttons</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="general_elements.html">General Elements</a></li>
+                    <li><a href="media_gallery.html">Media Gallery</a></li>
+                    <li><a href="typography.html">Typography</a></li>
+                    <li><a href="icons.html">Icons</a></li>
+                    <li><a href="glyphicons.html">Glyphicons</a></li>
+                    <li><a href="widgets.html">Widgets</a></li>
+                    <li><a href="invoice.html">Invoice</a></li>
+                    <li><a href="inbox.html">Inbox</a></li>
+                    <li><a href="calendar.html">Calendar</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="tables.html">Tables</a></li>
+                    <li><a href="tables_dynamic.html">Table Dynamic</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="chartjs.html">Chart JS</a></li>
+                    <li><a href="chartjs2.html">Chart JS2</a></li>
+                    <li><a href="morisjs.html">Moris JS</a></li>
+                    <li><a href="echarts.html">ECharts</a></li>
+                    <li><a href="other_charts.html">Other Charts</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
+                    <li><a href="fixed_footer.html">Fixed Footer</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div class="menu_section">
+              <h3>Live On</h3>
+              <ul class="nav side-menu">
+                <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="e_commerce.html">E-commerce</a></li>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="project_detail.html">Project Detail</a></li>
+                    <li><a href="contacts.html">Contacts</a></li>
+                    <li><a href="profile.html">Profile</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="page_403.html">403 Error</a></li>
+                    <li><a href="page_404.html">404 Error</a></li>
+                    <li><a href="page_500.html">500 Error</a></li>
+                    <li><a href="plain_page.html">Plain Page</a></li>
+                    <li><a href="login.html">Login Page</a></li>
+                    <li><a href="pricing_tables.html">Pricing Tables</a></li>
+                  </ul>
+                </li>
+                <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                      <li><a href="#level1_1">Level One</a>
+                      <li><a>Level One<span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                          <li class="sub_menu"><a href="level2.html">Level Two</a>
+                          </li>
+                          <li><a href="#level2_1">Level Two</a>
+                          </li>
+                          <li><a href="#level2_2">Level Two</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a href="#level1_2">Level One</a>
+                      </li>
+                  </ul>
+                </li>
+                <li><a href="javascript2:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+              </ul>
+            </div>
+
+          </div>
+          <!-- /sidebar menu -->
+      </div>
+      <script2 src="../../static/vendors/jquery/dist/jquery.min.js"></script2>
+    <!-- Bootstrap -->
+    <script2 src="../../static/vendors/bootstrap/dist/js/bootstrap.min.js"></script2>
+    <!-- FastClick -->
+    <script2 src="../../static/vendors/fastclick/lib/fastclick.js"></script2>
+    <!-- NProgress -->
+    <script2 src="../../static/vendors/nprogress/nprogress.js"></script2>
+    <!-- Chart.js -->
+    <script2 src="../../static/vendors/Chart.js/dist/Chart.min.js"></script2>
+    <!-- gauge.js -->
+    <script2 src="../../static/vendors/gauge.js/dist/gauge.min.js"></script2>
+    <!-- bootstrap-progressbar -->
+    <script2 src="../../static/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script2>
+    <!-- iCheck -->
+    <script2 src="../../static/vendors/iCheck/icheck.min.js"></script2>
+    <!-- Skycons -->
+    <script2 src="../../static/vendors/skycons/skycons.js"></script2>
+    <!-- Flot -->
+    <script2 src="../../static/vendors/Flot/jquery.flot.js"></script2>
+    <script2 src="../../static/vendors/Flot/jquery.flot.pie.js"></script2>
+    <script2 src="../../static/vendors/Flot/jquery.flot.time.js"></script2>
+    <script2 src="../../static/vendors/Flot/jquery.flot.stack.js"></script2>
+    <script2 src="../../static/vendors/Flot/jquery.flot.resize.js"></script2>
+    <!-- Flot plugins -->
+    <script2 src="../../static/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script2>
+    <script2 src="../../static/vendors/flot-spline/js/jquery.flot.spline.min.js"></script2>
+    <script2 src="../../static/vendors/flot.curvedlines/curvedLines.js"></script2>
+    <!-- DateJS -->
+    <script2 src="../../static/vendors/DateJS/build/date.js"></script2>
+    <!-- JQVMap -->
+    <script2 src="../../static/vendors/jqvmap/dist/jquery.vmap.js"></script2>
+    <script2 src="../../static/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script2>
+    <script2 src="../../static/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script2>
+    <!-- bootstrap-daterangepicker -->
+    <script2 src="../../static/vendors/moment/min/moment.min.js"></script2>
+    <script2 src="../../static/vendors/bootstrap-daterangepicker/daterangepicker.js"></script2>
+
+    <!-- Custom Theme script2s -->
+    <script2 src="../../static/js/custom.min.js"></script2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+<style lang="scss">
+@import '../../static/css/custom.css';
+@import '../../static/vendors/bootstrap/dist/css/bootstrap.min.css';
+@import '../../static/vendors/font-awesome/css/font-awesome.min.css';
+@import '../../static/vendors/nprogress/nprogress.css';
+@import '../../static/vendors/iCheck/skins/flat/green.css';
+@import '../../static/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css';
+@import '../../static/vendors/jqvmap/dist/jqvmap.min.css';
+@import '../../static/vendors/bootstrap-daterangepicker/daterangepicker.css';
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#myPage">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#services">SERVICES</a></li>
+            <li><a href="#portfolio">PORTFOLIO</a></li>
+            <li><a href="#pricing">PRICING</a></li>
+            <li><router-link to="/dashboard">Dashboard</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h1>Company</h1>
+    <script2 src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script2>
+    <script2 src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js" type="text/javascript"></script2>
+</div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+<style lang="scss">
+@import '../../static/css/home.css';
+</style>
